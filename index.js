d1cae12a024fc3209b3366123f05f48b19b21302
camario25 <camario25@yahoo.com>
2018-04-18 15:47:03 -0700

    starting to linksequelize to project

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const sqlite = require('sqlite'),
       express = require('express'),
       app = express();
 
+const sequelize = new Sequelize({
+  database: null,
+  username: null,
+  password: null,
+  dialect: 'sqlite',
+  storage: './db/database.db'
+});
+
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
 // START SERVER
@@ -11,6 +19,25 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
+
+const Film = sequelize.define('film', {
+  title: Sequelize.STRING,
+  release_date: Sequelize.DATE,
+  tagline: Sequelize.STRING,
+  revenue: Sequelize.BIGINT,
+  budget: Sequelize.BIGINT,
+  runtime: Sequelize.INTEGER,
+  original_language: Sequelize.STRING,
+  status: Sequelize.STRING,
+  genre_id: Sequelize.INTEGER
+})
+sequelize.sync()
+  .then(() => {
+    console.log('hello');
+  });
+// Films.findAll().then(films => {
+//   console.log(films);
+// })
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
